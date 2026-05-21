ffd339f4af0364f444e64c60c6968051785eaa3a
FakhrElddin <fakhereldeanaymanq@gmail.com>
2026-05-21 16:08:28 +0300

    implement dependency injection

--- lib/bloc/cubit.dart
+++ lib/bloc/cubit.dart
@@ -2,32 +2,16 @@ import 'package:bloc/bloc.dart';
 import 'package:news_app/bloc/states.dart';
 import 'package:news_app/models/news_response_model.dart';
 import 'package:news_app/models/sources_response_model.dart';
-import 'package:news_app/repository/home_repo.dart';
-import 'package:news_app/repository/news/data_source/news_remote_data_source.dart';
-import 'package:news_app/repository/news/data_source/news_remote_data_source_impl.dart';
 import 'package:news_app/repository/news/respository/news_repository.dart';
-import 'package:news_app/repository/news/respository/news_repository_impl.dart';
-import 'package:news_app/repository/source/data_sources/source_remote_data_source.dart';
-import 'package:news_app/repository/source/data_sources/source_remote_data_source_impl.dart';
 import 'package:news_app/repository/source/repository/source_repository.dart';
-import 'package:news_app/repository/source/repository/source_repository_impl.dart';
 
 class HomeCubit extends Cubit<HomeStates> {
-  //HomeCubit(this.homeRepo) : super(HomeInitState());
-  HomeCubit() : super(HomeInitState()){
-    sourceRemoteDataSource = SourceRemoteDataSourceImpl();
-    sourceRepository = SourceRepositoryImpl(remoteDataSource: sourceRemoteDataSource);
-    newsRemoteDataSource = NewsRemoteDataSourceImpl();
-    newsRepository = NewsRepositoryImpl(remoteDataSource: newsRemoteDataSource);
-  }
+  HomeCubit({required this.sourceRepository, required this.newsRepository}) : super(HomeInitState());
   SourcesResponseModel? sourcesResponse;
   NewsResponseModel? newsResponse;
   int selectedIndex = 0;
-  //HomeRepo homeRepo;
   late SourceRepository sourceRepository;
-  late SourceRemoteDataSource sourceRemoteDataSource;
   late NewsRepository newsRepository;
-  late NewsRemoteDataSource newsRemoteDataSource;
 
   void changeSelectedTab({required int index})async{
     selectedIndex = index;
--- lib/widgets/tabs_section.dart
+++ lib/widgets/tabs_section.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/bloc/cubit.dart';
 import 'package:news_app/bloc/states.dart';
-import 'package:news_app/repository/home_repo_implementation.dart';
+import 'package:news_app/dependency%20injection/di.dart';
 import 'package:news_app/widgets/news_widget.dart';
 
 class TabsSection extends StatelessWidget {
@@ -16,8 +16,11 @@ class TabsSection extends StatelessWidget {
 
 
     return BlocProvider(
-      create: (context) => HomeCubit()..getSources(categoryName: categoryName),
-      //create: (context) => HomeCubit(HomeRepoImplementation())..getSources(categoryName: categoryName),
+      create: (context) => HomeCubit(
+        sourceRepository: injectSourceRepository(),
+        newsRepository: injectNewsRepository(),
+      )
+        ..getSources(categoryName: categoryName),
       child: BlocConsumer<HomeCubit, HomeStates>(
         listener: (context, state) {
           if(state is GetSourcesErrorState){
